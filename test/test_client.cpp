c1906046bc14b4d75399e96484102ea9a9cbd001
mingjitianming <626670628@qq.com>
2020-12-18 10:18:39 +0800

    add_custom_target to generate message

--- test/test_client.cpp
+++ test/test_client.cpp
@@ -7,10 +7,10 @@ int main()
     std::string config = "/home/zmy/project_ws/my_hk_transmit/config/plugins.yaml";
     std::string ip("127.0.0.1");
 
-    std::shared_ptr<Client> client = Client::create(config, "zmy");
-    std::dynamic_pointer_cast<PlcTransmit>(client->getMethod("plc"))
+    std::shared_ptr<transmit::Client> client = transmit::Client::create(config, "zmy");
+    std::dynamic_pointer_cast<transmit::plugins::PlcTransmit>(client->getMethod("plc"))
         ->setClientProcess(
-            [](int a, int b) {
+            [](int a, int b, cv::Point2f c) {
                 std::cout << a + b << std::endl;
             });
     client->start(ip, 8001);
